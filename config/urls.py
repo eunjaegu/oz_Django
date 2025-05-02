9a3bbd2d42aaa0ce5ab88d1d7de641691a513daf
eunjaegu <kuhj1021@gmail.com>
2025-05-02 14:49:33 +0900

    ✨ Challenge 02
    todo_list_apps

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-#from todoList import views
 from todoList import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', views.todo_list),
