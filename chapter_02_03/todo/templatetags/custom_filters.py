9c5f7217e89e124bc375587e339cd7570698520f
eunjaegu <kuhj1021@gmail.com>
2025-05-14 00:17:46 +0900

    ✨ Update CBV
    Update custom_filter.py

--- chapter_02_03/todo/templatetags/custom_filters.py
+++ chapter_02_03/todo/templatetags/custom_filters.py
@@ -1,10 +1,4 @@
 from django import template
-
-register = template.Library()
-
-# todo/templatetags/custom_filters.py
-
-from django import template
 from django.apps import apps
 
 register = template.Library()
