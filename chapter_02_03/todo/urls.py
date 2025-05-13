95946e9bb93d48622dcd0c5b01a87bfbb4462ecc
eunjaegu <kuhj1021@gmail.com>
2025-05-14 01:54:30 +0900

    ✨ Add chapter05
    add comment, BootStrap5, pagination

--- chapter_02_03/todo/urls.py
+++ chapter_02_03/todo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from todo import cb_views
+from todo.cb_views import CommentCreateView, CommentDeleteView, CommentUpdateView
 
 app_name = 'todo'
 
@@ -10,5 +11,8 @@ urlpatterns = [
     path('create/', cb_views.TodoCreateView.as_view(), name='cbv_todo_create'),
     path('<int:pk>/update/', cb_views.TodoUpdateView.as_view(), name='cbv_todo_update'),
     path('<int:pk>/delete/', cb_views.TodoDeleteView.as_view(), name='cbv_todo_delete'),
+    path('comment/<int:todo_id>/create/',  CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/delete/',  CommentDeleteView.as_view(), name='comment_delete'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update')
 
 ]
