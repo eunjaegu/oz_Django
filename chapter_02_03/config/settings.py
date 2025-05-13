11e1180d068e7210939636df469e27b1d956cc96
eunjaegu <kuhj1021@gmail.com>
2025-05-14 01:54:30 +0900

    ✨ Add chapter06
    add comment, BootStrap5, pagination

--- chapter_02_03/config/settings.py
+++ chapter_02_03/config/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -124,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 LOGIN_URL = '/accounts/login/'
 #LOGIN_REDIRECT_URL = '/fb/todo/'
 LOGIN_REDIRECT_URL = '/'
--- chapter_02_03/todo/forms.py
+++ chapter_02_03/todo/forms.py
@@ -1,15 +1,42 @@
 from django import forms
 
-from todo.models import Todo
+from todo.models import Todo, Comment
 
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'description', 'start_date', 'end_date', 'is_complete']
-            #'__all__'
+        labels = {
+            'title': '할 일 제목',
+            'description': '상세 내용',
+            'start_date': '시작 날짜',
+            'end_date': '마감 날짜',
+            'is_complete': '완료 여부',
+        }
 
 class TodoUpdateForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'description', 'start_date', 'end_date', 'is_complete']
+        labels = {
+            'title': '할 일 제목',
+            'description': '상세 내용',
+            'start_date': '시작일',
+            'end_date': '종료일',
+            'is_complete': '완료 여부',
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['message', ]
+        labels = {
+            'message': '내용',
+        }
+        widgets = {
+            'message': forms.Textarea(attrs={
+                'rows': 3, 'cols': 40, 'class': 'form-control', 'placeholder': '댓글 내용을 입력해주세요.'
+            }),
+        }
